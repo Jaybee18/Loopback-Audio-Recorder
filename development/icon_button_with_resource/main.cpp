e56352d1d9a6fe811b4ceabd2541cea4011c33aa
Jan Bessler <mister.jaybee18@gmail.com>
2025-05-09 12:56:24 +0200

    got compiling icons with resource files working

---
+++ development/icon_button_with_resource/main.cpp
@@ -0,0 +1,100 @@
+/*
+    A file called "myicon.ico" should be present in the same directory as the .exe file.
+    Compile with: g++ main.cpp
+*/
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <windows.h>
+#include <CommCtrl.h>
+#include "resource.h"
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
+{
+    // Register the window class.
+    const wchar_t CLASS_NAME[]  = L"Sample Window Class";
+    
+    WNDCLASS wc = { };
+
+    wc.lpfnWndProc   = WindowProc;
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Create the window.
+    HWND hwnd = CreateWindowEx(
+        0,
+        CLASS_NAME,
+        L"Icon Button Example",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL, 
+        NULL,
+        hInstance,
+        NULL
+    );
+    
+    HWND hwndButton = CreateWindow(
+        L"BUTTON",
+        L"",
+        WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON | BS_ICON,
+        10,
+        10,
+        50,
+        50,
+        hwnd,
+        (HMENU)101,
+        NULL,
+        nullptr
+    );
+    HICON myIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MYICON));
+    SendMessage(hwndButton, BM_SETIMAGE, IMAGE_ICON, (LPARAM) myIcon);
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    // Run the message loop.
+
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+
+            // All painting occurs here, between BeginPaint and EndPaint.
+
+            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
+
+            EndPaint(hwnd, &ps);
+        }
+        return 0;
+
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
